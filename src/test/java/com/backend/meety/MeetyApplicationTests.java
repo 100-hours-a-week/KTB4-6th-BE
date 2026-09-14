85ca30971f999b737c58e001c4a935ce71ff17bd
h26n <likelion.leedh@gmail.com>
2026-09-14 19:06:09 +0900

    init

---
+++ src/test/java/com/backend/meety/MeetyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.meety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
