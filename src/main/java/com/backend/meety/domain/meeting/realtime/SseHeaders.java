34f26106ad10e1255b2581ded56851ad8d37d270
h26n <likelion.leedh@gmail.com>
2026-09-24 14:26:58 +0900

    fix: SSE 응답에 nginx 버퍼링 차단 헤더 추가

---
+++ src/main/java/com/backend/meety/domain/meeting/realtime/SseHeaders.java
@@ -0,0 +1,10 @@
+package com.backend.meety.domain.meeting.realtime;
+
+public final class SseHeaders {
+
+    public static final String X_ACCEL_BUFFERING = "X-Accel-Buffering";
+    public static final String X_ACCEL_BUFFERING_OFF = "no";
+
+    private SseHeaders() {
+    }
+}
